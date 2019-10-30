9bbb3264bd22bf91af72b2cfc41ea79ef40c8ecd
Kevin <kevinm1776@gmail.com>
2019-10-30 14:04:32 -0700

    Turn histogram into a class.

--- code/histogram.py
+++ code/histogram.py
@@ -1,40 +1,45 @@
 from collections import defaultdict
 
-def open_file(filepath):
-    f = open(filepath, 'r')
-    for line in f:
-        for word in line:
-            yield word
+
+class Histogram:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.unique_words = 0
+        self.hist = defaultdict(lambda: _incrememnt_unique())
+
+    def _incrememnt_unique():
+        self.unique_words += 1
+        return 0
+
+    def _open_file(self):
+        f = open(self.filepath, 'r')
+        for line in f:
+            for word in line:
+                yield word
+
+    def build_histogram(self):
+        for word in self._open_file():
+            self.hist[word] += 1
+
+    def get_frequency(word):
+        return self.hist[word]
 
 
-def histogram(filepath):
-    hist = defaultdict(lambda: 0)
-    for word in open_file(filepath):
-        hist[word] += 1
 
-    return hist
+'''
+    def hist_list(filepath):
+        hist = []
+        index = 0
+        ids = {}
+        for word in enumerate(open_file):
+            if word not in ids:
+                ids[index] = word
 
-def hist_list(filepath):
-    hist = []
-    index = 0
-    ids = {}
-    for word in enumerate(open_file):
-        if word not in ids:
-            ids[index] = word
+            if len(hist) >= index:
+                hist[index] += 1
 
-        if len(hist) >= index:
-            hist[index] += 1
+            else:
+                hist.append(0)
 
-        else:
-            hist.append(0)
-
-    return hist, ids
-
-
-def unique_words(hist):
-    return len(hist)
-
-def frequency(word, hist):
-    return hist[word]
-
-
+        return hist, ids
+'''
