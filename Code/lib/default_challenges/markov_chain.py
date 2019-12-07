f56897c12bd246411d6761109f0daf9fbe6d032f
Kevin <kevinm1776@gmail.com>
2019-12-06 17:38:59 -0800

    In progress starting nth order markov chain using circular buffer.

--- Code/lib/default_challenges/markov_chain.py
+++ Code/lib/default_challenges/markov_chain.py
@@ -1,4 +1,4 @@
-
+from data_struct_algo.queues.circular_buffer import CircularBuffer
 
 START_TOKEN = '#START'
 END_TOKEN = '#END'
@@ -7,8 +7,8 @@ END_TOKEN = '#END'
 class MarkovChain(dict):
     '''MarkovChain is a dictionary implementation of a markov chain.'''
 
-    def __init__(self):
-        super(MarkovChain, self).__init__()  # Initialize as a dict
+    def __init__(self, n=1):
+        self.n = n
 
     def __missing__(self, key):
         self[key] = Dictogram()
@@ -19,32 +19,51 @@ class MarkovChain(dict):
     def get_words_list(path_to_file):
         f = open(path_to_file, 'r')
         for line in f:
+            yield START_TOKEN
             for word in line.split():
                 yield word.strip()
 
             yield END_TOKEN
 
     def build_markov(self, path_to_file):
-        previous = START_TOKEN
-        for word in self.get_words_list(path_to_file):
-            self[previous].add_count(word)
+        previous_n = CircularBuffer(fixed_size=self.n)
+        words = self.get_words_list(path_to_file)
 
-            previous = word
+        for word in words:
+            if word is START_TOKEN:
+                previous_n.empty()
+                for _ in range(self.n):
+                    previous_n.enqueue(next(words))
+
+                self[START_TOKEN].add_count(previous_n)
+                print(previous_n)
+                continue
+
+            self[previous_n].add_count(word)
+            previous_n.enqueue(word)
 
     def generate_sentence(self):
-        word = START_TOKEN
+        previous_n = self[START_TOKEN].sample()
+
         count = 0
-        while word != END_TOKEN and count <= 20:
-            word = self[word].sample()
+        while count <= 20:
+            word = self[previous_n].sample()
+            if word is END_TOKEN:
+                break
+
             count += 1
+            previous_n.enqueue(word)
             yield word
 
 
 if __name__ == '__main__':
     from dictogram import Dictogram
-    mv = MarkovChain()
+    mv = MarkovChain(2)
     mv.build_markov('data/test.txt')
-    print(list(mv.generate_sentence()))
+    #for word in mv.get_words_list('data/test.txt'):
+     #   print(word)
+    print(mv[START_TOKEN])
+    #print(list(mv.generate_sentence()))
 
 else:
     from .dictogram import Dictogram
